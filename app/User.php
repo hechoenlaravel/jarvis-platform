f46e1cd9abfd8477cb0e9dc4990d88cc2c7bfcf0
Jose Luis Fonseca <jose@ditecnologia.com>
2015-09-04 14:43:13 -0500

    update user Model

--- app/User.php
+++ app/User.php
@@ -2,36 +2,8 @@
 
 namespace JarvisPlatform;
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
-use Joselfonseca\LaravelAdmin\Traits\LaravelAdminUser;
+use Joselfonseca\LaravelAdmin\Services\Users\User as Model;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract
-{
+class User extends Model {
 
-    use Authenticatable, CanResetPassword, LaravelAdminUser;
-
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['name', 'email', 'password'];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = ['password', 'remember_token'];
 }
