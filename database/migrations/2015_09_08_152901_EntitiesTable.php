f454a44ec198538f14db26809db492d7b36123ed
Jose Luis Fonseca <jose@ditecnologia.com>
2015-09-08 16:12:56 -0500

    migration for Entities

---
+++ database/migrations/2015_09_08_152901_EntitiesTable.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EntitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_entities', function($table){
+            $table->increments('id');
+            $table->string('namespace');
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description');
+            $table->string('prefix');
+            $table->boolean('locked')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_entities');
+    }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 ## Jarvis Platform
+
+[![Build Status](https://travis-ci.org/hechoenlaravel/jarvis-platform.svg)](https://travis-ci.org/hechoenlaravel/jarvis-platform)
