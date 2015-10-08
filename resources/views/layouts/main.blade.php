7c14e1591bbeb65d93f4d67b0f7b7c984552938c
jose fonseca <jose@ditecnologia.com>
2015-10-08 18:18:25 -0500

    Extract Meta to partials

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,32 +4,10 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('pageTitle')</title>
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-        <link href="{{ asset('css/all.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/jarvis-platform.css') }}" rel="stylesheet">
-        @yield('styles')
-        <!-- Fonts -->
-        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-        <script>
-            var GLOBALS = {
-                site_url: '{{url()}}',
-                datablesLang: '//cdn.datatables.net/plug-ins/1.10.7/i18n/Spanish.json'
-            };
-        </script>
+        @include('partials.meta')
     </head>
     <body class="login-page">
         @yield('content')
-        <!-- Scripts -->
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"></script>
-        <script src="{{ asset('js/all.js') }}"></script>
-        @yield('scripts')
+        @include('partials.footer_meta')
     </body>
 </html>
