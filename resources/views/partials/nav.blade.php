a064f3632822097caa1a2a48d1a50ad50958f358
Jose Luis Fonseca <jose@ditecnologia.com>
2015-11-04 09:31:18 -0500

    update Menus and responsive

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,14 +1,41 @@
 <header class="main-header">
     <a href="{{url('dashboard')}}" class="logo">
-        {{config('app.app_name')}}
+        <span class="logo-mini">{{substr(config('app.app_name'), 0, 3)}}</span>
+        <span class="logo-lg">{{config('app.app_name')}}</span>
     </a>
-    <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top" role="navigation">
-        {!! MenuPing::render('navbar') !!}
-        <!-- Navbar Right Menu -->
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Ver Menu</span>
+        </a>
         <div class="navbar-custom-menu">
-            @include('jarvisPlatform::notifications.navbar-notifications')
-            {!! MenuPing::render('userMenu') !!}
+            <ul class="nav navbar-nav">
+                @include('jarvisPlatform::notifications.navbar-notifications')
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                        <img src="{{Auth::user()->getAvatarImageUrl()}}" class="user-image" alt="User Image">
+                        <span class="hidden-xs">{{Auth::user()->name}}</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <!-- User image -->
+                        <li class="user-header">
+                            <img src="{{Auth::user()->getAvatarImageUrl()}}" class="img-circle" alt="User Image">
+                            <p>
+                                {{Auth::user()->name}}
+                            </p>
+                        </li>
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <div class="pull-left">
+                                <a href="{{url('me/edit')}}" class="btn btn-default btn-flat">Editar Perfil</a>
+                            </div>
+                            <div class="pull-right">
+                                <a href="{{url('auth/logout')}}" class="btn btn-default btn-flat">Salida Segura</a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
         </div>
     </nav>
 </header>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,15 +1,17 @@
-<section class="sidebar">
-    <div class="user-panel">
-        <div class="pull-left image">
-            <img src="{{Auth::user()->getAvatarImageUrl()}}" class="img-circle" alt="{{Auth::user()->name}}">
+<aside class="main-sidebar">
+    <section class="sidebar">
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{Auth::user()->getAvatarImageUrl()}}" class="img-circle" alt="{{Auth::user()->name}}">
+            </div>
+            <div class="pull-left info">
+                <p>{{Auth::user()->name}}</p>
+            </div>
         </div>
-        <div class="pull-left info">
-            <p>{{Auth::user()->name}}</p>
-        </div>
-    </div>
-    <ul class="sidebar-menu">
-        <li class="header">Menu</li>
-    </ul>
-    {!! MenuPing::render('sidebar') !!}
-    {!! MenuPing::render('config') !!}
-</section>
+        <ul class="sidebar-menu">
+            <li class="header">Menu</li>
+        </ul>
+        {!! MenuPing::render('sidebar') !!}
+        {!! MenuPing::render('config') !!}
+    </section>
+</aside>
