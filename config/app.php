c384146484fa3f006b5dc42be7b7efc2f414d431
Jose Luis Fonseca <jose@ditecnologia.com>
2015-12-02 15:33:50 -0500

    fix version.

--- config/app.php
+++ config/app.php
@@ -183,7 +183,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
     ],
 
-    'version' => env('APP_VERSION', '1.1.0'),
+    'version' => env('APP_VERSION', '1.0.3'),
     'app_name' => env('APP_NAME', 'Jarvis Platform Demo Skeleton App')
 
 ];
