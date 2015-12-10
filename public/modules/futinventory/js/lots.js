8f50311ad7770cb9ace4b8a1bc748d59f8e0f124
Jose Luis Fonseca <jose@ditecnologia.com>
2015-12-09 22:43:48 -0500

    update the lots JS and remove some dev-packages

--- public/modules/futinventory/js/lots.js
+++ public/modules/futinventory/js/lots.js
@@ -1,26 +1,36 @@
 JarvisPlatform.controller('LotsController', ['$scope', 'LotsService', function ($scope, LotsService) {
 
-    LotsService.getLots({}).success(function(data){
-        $scope.loading = 0;
-        $scope.NoResults = 1;
-        if(data.meta.pagination.count > 0)
-        {
-            $scope.NoResults = 0;
-            $scope.hasMoreResults = false;
-            if(data.meta.pagination.links.next !== undefined)
+    $scope.lotParams = {};
+
+    $scope.searchLots = function()
+    {
+        $('#searchLots').button('loading');
+        LotsService.getLots($scope.lotParams).success(function(data){
+            $('#searchLots').button('reset');
+            $scope.loading = 0;
+            $scope.NoResults = 1;
+            if(data.meta.pagination.count > 0)
             {
-                $scope.hasMoreResults = true;
-                $scope.nextPage = data.meta.pagination.current_page + 1;
-                $scope.prevPage = data.meta.pagination.current_page - 1;
+                $scope.NoResults = 0;
+                $scope.hasMoreResults = false;
+                if(data.meta.pagination.links.next !== undefined)
+                {
+                    $scope.hasMoreResults = true;
+                    $scope.nextPage = data.meta.pagination.current_page + 1;
+                    $scope.prevPage = data.meta.pagination.current_page - 1;
+                }
+                $scope.current_page = data.meta.pagination.current_page;
+                $scope.total_pages = data.meta.pagination.total_pages;
+                $scope.total = data.meta.pagination.total;
+            }else{
+                $scope.total = 0;
             }
-            $scope.current_page = data.meta.pagination.current_page;
-            $scope.total_pages = data.meta.pagination.total_pages;
-            $scope.total = data.meta.pagination.total;
-        }else{
-            $scope.total = 0;
-        }
-        $scope.lots = data.data;
-    });
+            $scope.lots = data.data;
+        }).error(function(data, status){
+            $('#searchLots').button('reset');
+            HandleErrorResponse(data, status);
+        });
+    }
 
 }]);
 /** Services **/
@@ -30,8 +40,8 @@ JarvisPlatform.factory('LotsService', ['$http', function ($http) {
         getLots : function(parameters)
         {
             return $http({
-                method: 'get',
-                url: GLOBALS.site_url+'/api/fut/inv/lots',
+                method: 'post',
+                url: GLOBALS.site_url+'/api/fut/inv/lots/search',
                 headers: {
                     'Content-Type' : 'application/json'
                 },
